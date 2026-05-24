7ca0d7d311efb3bf0fee0d1f0cd7461b94bc8b07
ooomangalcraft-hash <ooomangalcraft@gmail.com>
2026-05-24 16:52:58 +0300

    Update config.py

--- config.py
+++ config.py
@@ -3,7 +3,7 @@ from dotenv import load_dotenv
 
 load_dotenv()
 
-BOT_TOKEN = os.getenv("BOT_TOKEN", "8833204061:AAHhI53Hk6-C2_f8B1gnBUPB5uf_AeMCsmY")
+BOT_TOKEN = os.getenv("BOT_TOKEN", "8833204061:AAFADcrkr6qUmRZZaFQ2iGtPUFx9gNe7bss")
 ADMIN_USERNAME = os.getenv("ADMIN_USERNAME", "SVKolosov")
 ESCALATION_KEYWORDS = ["оператор", "человек", "менеджер", "помощь", "живой"]
 CONFIDENCE_THRESHOLD = float(os.getenv("CONFIDENCE_THRESHOLD", "0.4"))
